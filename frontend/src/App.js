cd5cb38724026d9655ad8102150e494924f46bea
toriqulmahal6359 <toriqulmahal6359@yahoo.com>
2022-12-06 13:18:45 +0600

    Header & Footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
+import WebFont from "webfontloader";
 import './App.css';
+import Header from "./component/layout/Header/Header.js";
+import { BrowserRouter as Router } from "react-router-dom";
+import Footer from './component/layout/Footer/Footer';
 
 function App() {
+  React.useEffect(() => {
+    WebFont.load({
+      google: {
+        families: ['Roboto', 'Droid Sans', 'Chilanka']
+      }
+    })
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+        <Header />
+        <Footer />
+    </Router>
+
   );
 }
 
---
+++ frontend/src/component/layout/Footer/Footer.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import "./Footer.css"
+import playStore from "../../../images/playStore.png"; 
+import appStore from "../../../images/appStore.png"; 
+
+const Footer = () => {
+  return (
+    <footer id="footer">
+        <div class="leftFooter">
+            <h4>Download Our App</h4>
+            <p>Download App For iOS and Android Phone</p>
+            <img src={playStore} alt='playStore'></img>
+            <img src={appStore} alt='appStore'></img>
+        </div>
+        <div class="midFooter">
+            <h1>ECOMMERCE</h1>
+            <p>High Quality is Our First Priority</p>
+
+            <p>Copyright 2021 &copy; Toriqul Mahal</p>
+        </div>
+        <div class="rightFooter">
+            <h4>Follow Us</h4>
+            <a href='javascript:void(0)'>Facebook</a>
+            <a href='javascript:void(0)'>Youtube</a>
+            <a href='javascript:void(0)'>Instagram</a>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
