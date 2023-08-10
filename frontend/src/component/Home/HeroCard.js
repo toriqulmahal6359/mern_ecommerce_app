43e3609f181b6b9cf51f3d9006b5e2300d4d4ea3
toriqulmahal6359 <toriqulmahal6359@yahoo.com>
2023-08-10 12:54:24 +0600

    v9.1_feature_added

--- frontend/src/component/Home/HeroCard.js
+++ frontend/src/component/Home/HeroCard.js
@@ -1,8 +1,9 @@
 import "./HeroCard.css";
 import React from 'react'
 import Carousel from 'react-material-ui-carousel';
-import { Paper } from '@material-ui/core';
+import { Link, Paper } from '@material-ui/core';
 import { CgMouse } from "react-icons/cg";
+import { IoMdPaperPlane } from "react-icons/io";
 
 const HeroCard = ({ products }) => {
   return (
@@ -11,18 +12,21 @@ const HeroCard = ({ products }) => {
             {  products?.map((product) => (
                 <Paper>
                     <div className='game-card-container'>
-                    <div className='game-card' style={{"--img": `url(${product.images[0].url})`}}>
+                    <div className='game-card' style={{"--img": `url(${product.backdrops[0].url})`}}>
                         <div className='game-detail'>
                             <div className='game-image'>
                                 <img src={product.images[0].url} alt={product.name} />
                             </div>
                             <div className="game-platform">
                                 <h4>Available for {product.category} now</h4>
-                                <a href='#container'>
-                                    <button>
-                                        Scroll <CgMouse />
-                                    </button>
-                                </a>
+                                <div className="explore-buttons">
+                                    <a href='#container'>
+                                        <button>Scroll <CgMouse /></button>
+                                    </a>
+                                    <a href="/products">
+                                        <button>Explore <IoMdPaperPlane /></button>
+                                    </a>
+                                </div>
                             </div>
                         </div>
                     </div>
